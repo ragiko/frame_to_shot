bc40eecd95bbc8814a634cb98d970fc7b9f1e44f
Taguchi Hiroaki <ragik0@Macintosh.local>
2015-06-26 16:21:06 +0900

    mission complete

--- shot.rb
+++ shot.rb
@@ -1,28 +1,48 @@
 require 'pp'
 
+# shot内にあるframeを選択
+def select_frame_in_shot(res, shot)
+    return res.select do |x|
+        shot[:video] == x[:video] \
+            and \
+            ( (shot[:ss] <= x[:ss] and x[:ss] <= shot[:es]) \
+            or (shot[:ss] <= x[:es] and x[:es] <= shot[:es]) )
+    end
+end
+
+# 候補からshot内の人と重みを選ぶ
 def choice_parson_and_weight(candidate_list)
     if candidate_list.size == 0
         return ""
     end
 
+    # フォーマットを変換
+    list = []
+    candidate_list.each do |item|
+        list <<  [
+            item[:parson],
+            item[:ss],
+            item[:es],
+            item[:weight],
+        ]
+    end
+
     # 1. 多数決
     parson_cnt = Hash.new(0)
-    candidate_list.map {|x| parson_cnt[x[0]] += 1}
+    list.map {|x| parson_cnt[x[0]] += 1}
     max_val = parson_cnt.max{ |x, y| x[1] <=> y[1] }[1]
     max_parsons = parson_cnt.select { |k, v| v == max_val }.keys
-    candidate_list = candidate_list.select {|x| max_parsons.include?(x[0])}
-    pp candidate_list
+    list = list.select {|x| max_parsons.include?(x[0])}
 
     # 2. 同じもの回数の場合重みで決定
     # グループ化して平均とって重みの最大のもの
     group_mean_weight = Hash.new() { |h,k| h[k] = [] }
-    candidate_list.each do |x| 
+    list.each do |x| 
         group_mean_weight[x[0]] << x[3]
     end 
     group_mean_weight.each do |k, v|
         group_mean_weight[k] = v.inject(0.0) {|sum, n| sum + n } / v.size
     end
-    pp group_mean_weight
 
     x = group_mean_weight.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }[0]
     return x
@@ -36,6 +56,7 @@ File.open("./all.shot").each_line do |l|
         video: a[0], 
         id: a[1].to_i,
         ss: a[2].to_f, 
+        es: a[3].to_f, 
     }
 end
 
@@ -43,18 +64,21 @@ end
 res = []
 File.open("./result2.csv").each_line do |l|
     a = l.split(",")
-    a = [a[0], a[1].to_f, a[2].to_f, a[3].delete("\n").to_f]
+    a = {
+        video: a[0],
+        parson: a[1],
+        ss: a[2].to_f,
+        es: a[3].to_f,
+        weight: a[4].delete("\n").to_f
+    }
     res << a
 end
 
 shots.each do |shot|
     # shotに挟まっているframeを取得
-    s = res.select do |x|
-        (shot[:ss] <= x[1] and x[1] <= shot[:es]) or (shot[:ss] <= x[2] and x[2] <= shot[:es])
-    end
     pp shot
-    choice_parson_and_weight(s)
+    s = select_frame_in_shot(res, shot)
+    # frame中の人と重みを決定
+    pp choice_parson_and_weight(s)
 end
 
-
-S_01FPVDB07010204_VIS_01FPVDB07010204_VIS_01
