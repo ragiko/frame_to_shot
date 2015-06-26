a0e567551a3fa28f26c5ac48f0deee2c706dba5a
Taguchi Hiroaki <ragik0@Macintosh.local>
2015-06-26 15:59:02 +0900

    機能ある程度完成

--- shot.rb
+++ shot.rb
@@ -1,5 +1,34 @@
 require 'pp'
 
+def choice_parson_and_weight(candidate_list)
+    if candidate_list.size == 0
+        return ""
+    end
+
+    # 1. 多数決
+    parson_cnt = Hash.new(0)
+    candidate_list.map {|x| parson_cnt[x[0]] += 1}
+    max_val = parson_cnt.max{ |x, y| x[1] <=> y[1] }[1]
+    max_parsons = parson_cnt.select { |k, v| v == max_val }.keys
+    candidate_list = candidate_list.select {|x| max_parsons.include?(x[0])}
+    pp candidate_list
+
+    # 2. 同じもの回数の場合重みで決定
+    # グループ化して平均とって重みの最大のもの
+    group_mean_weight = Hash.new() { |h,k| h[k] = [] }
+    candidate_list.each do |x| 
+        group_mean_weight[x[0]] << x[3]
+    end 
+    group_mean_weight.each do |k, v|
+        group_mean_weight[k] = v.inject(0.0) {|sum, n| sum + n } / v.size
+    end
+    pp group_mean_weight
+
+    x = group_mean_weight.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }[0]
+    return x
+end
+
+# get shot deta
 shots = []
 File.open("./all.shot").each_line do |l|
     a = l.split(" ")
@@ -7,31 +36,25 @@ File.open("./all.shot").each_line do |l|
         video: a[0], 
         id: a[1].to_i,
         ss: a[2].to_f, 
-        es: a[3].to_f, 
     }
 end
 
+# get result deta
 res = []
-File.open("./reslut.csv").each_line do |l|
+File.open("./result2.csv").each_line do |l|
     a = l.split(",")
     a = [a[0], a[1].to_f, a[2].to_f, a[3].delete("\n").to_f]
     res << a
 end
 
-shot = shots[0]
-s = res.select do |x|
-    (shot[:ss] <= x[1] and x[1] <= shot[:es]) or (shot[:ss] <= x[2] and x[2] <= shot[:es])
+shots.each do |shot|
+    # shotに挟まっているframeを取得
+    s = res.select do |x|
+        (shot[:ss] <= x[1] and x[1] <= shot[:es]) or (shot[:ss] <= x[2] and x[2] <= shot[:es])
+    end
+    pp shot
+    choice_parson_and_weight(s)
 end
 
-# 1. 多数決
-# 2. 同じもの回数の場合重みで決定
-parson_cnt = Hash.new(0)
-s.map {|x| parson_cnt[x[0]] += 1}
-max_val = parson_cnt.max{ |x, y| x[1] <=> y[1] }[1]
-max_parsons = parson_cnt.select { |k, v| v == max_val }.keys
-x = s.select {|x| max_parsons.include?(x[0])}
 
-x = x.sort {|a, b|
-     -1 * (a[3] <=> b[3])
-}[0][0]
-
+S_01FPVDB07010204_VIS_01FPVDB07010204_VIS_01
